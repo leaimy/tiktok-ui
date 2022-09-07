679a6f7321cb6d3a28da4788c0f7c463dc14761b
Nguyễn Thị Hà <hantctk42@gmail.com>
2022-09-07 16:18:43 +0700

    Bổ sung cấu hình Router/Layout cho dự án 🍑🍐🍏

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
 import { DefaultLayout } from '~/components/Layout';
@@ -8,7 +9,7 @@ function App() {
             <div className="App">
                 <Routes>
                     {publicRoutes.map((route, index) => {
-                        const Layout = route.layout || DefaultLayout;
+                        const Layout = route.layout === null ? Fragment : DefaultLayout;
                         const Page = route.component;
                         return (
                             <Route
