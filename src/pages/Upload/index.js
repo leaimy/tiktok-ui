8dfe348a8e990135e21481f0953aef6c6695cae9
Nguyễn Thị Hà <hantctk42@gmail.com>
2022-09-08 08:18:48 +0700

    Bổ sung cấu hình Router/Layout cho dự án 🍑🍐🍏

--- src/pages/Upload/index.js
+++ src/pages/Upload/index.js
@@ -1,14 +1,5 @@
-import Header from '~/components/Layout/DefaultLayout/Header';
-
 function Upload() {
-    return (
-        <div>
-            <Header />
-            <div className="content">
-                <h2>Upload page</h2>
-            </div>
-        </div>
-    );
+    return <h2>Upload page</h2>;
 }
 
 export default Upload;
