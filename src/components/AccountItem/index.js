ec36ea38daa72f3c7da873c5995fee5ccfa698ad
Nguyễn Thị Hà <hantctk42@gmail.com>
2022-09-09 10:24:37 +0700

    Xây dựng UI phần Header #2 🗼🗼🏰

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/b8454f8b4661d44e496524559a24ea31~c5_100x100.jpeg?x-expires=1662861600&x-signature=gRPp8GqbrhUWalAjBcthbirtWfc%3D"
+                alt="Hoa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyễn Thị Hoa</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenthihoa</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
