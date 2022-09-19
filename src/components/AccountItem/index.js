88d703eb4e868f1c45a3358c18b5cfa952c900e8
Nguyễn Thị Hà <hantctk42@gmail.com>
2022-09-19 11:11:28 +0700

    Xây dựng Logic phần Header #2 😊😉😊

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -3,12 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
 import Image from '~/components/Image';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
 function AccountItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
             <Image className={cx('avatar')} src={data.avatar} alt={data.full_name} />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
@@ -17,7 +18,7 @@ function AccountItem({ data }) {
                 </h4>
                 <span className={cx('username')}>{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
