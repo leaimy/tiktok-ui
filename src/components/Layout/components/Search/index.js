4e8deb5b3d8a5c35c5fdd2ee6ae8078a509fb95c
Nguyễn Thị Hà <hantctk42@gmail.com>
2022-09-19 10:50:13 +0700

    Xây dựng Logic phần Header #2 🍥🍥🍥

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -15,19 +15,27 @@ function Search() {
     const [searchValue, setSearchValue] = useState('');
     const [searchResult, setSearchResult] = useState([]);
     const [showResult, setShowResult] = useState(true);
+    const [loading, setLoading] = useState(false);
 
     const inputRef = useRef();
 
     useEffect(() => {
         if (!searchValue.trim()) {
+            setSearchResult([]);
             return;
         }
+
+        setLoading(true);
 
         fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
             .then((res) => res.json())
             .then((res) => {
                 setSearchResult(res.data);
                 // console.log(res.data[0].id);
+                setLoading(false);
+            })
+            .catch(() => {
+                setLoading(false);
             });
     }, [searchValue]);
 
@@ -64,12 +72,12 @@ function Search() {
                     onChange={(e) => setSearchValue(e.target.value)}
                     onFocus={() => setShowResult(true)}
                 />
-                {!!searchValue && (
+                {!!searchValue && !loading && (
                     <button className={cx('clear')} onClick={handleClear}>
                         <FontAwesomeIcon icon={faCircleXmark} />
                     </button>
                 )}
-                {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
+                {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
 
                 <button className={cx('search-btn')}>
                     <SearchIcon />
