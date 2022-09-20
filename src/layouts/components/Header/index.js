24df2c9b4b8148c5acbf27a606d4051e4dfd33ff
Nguyễn Thị Hà <hantctk42@gmail.com>
2022-09-20 14:39:32 +0700

    Tái cấu trúc và tối ưu code #1 🍉🍓🍎

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -12,7 +12,7 @@ import Menu from '~/components/Popper/Menu';
 import Image from '~/components/Image';
 
 import Search from '~/layouts/components/Search';
-import routesConfig from '~/config/routes';
+import config from '~/config';
 
 import {
     CoinsIcon,
@@ -94,7 +94,7 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <Link to={routesConfig.home} className={cx('logo-link')}>
+                <Link to={config.routes.home} className={cx('logo-link')}>
                     <img src={images.logo} alt="Tiktok" />
                 </Link>
                 <Search />
