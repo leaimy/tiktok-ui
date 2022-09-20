abf3403b5905ab2b7bd18796f93f2422c33920a7
Nguyễn Thị Hà <hantctk42@gmail.com>
2022-09-20 08:48:15 +0700

    Sửa lỗi và hoàn thiện phần Header #1 🍃🍃🍃

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -51,6 +51,13 @@ function Search() {
     const handleHideResult = () => {
         setShowResult(false);
     };
+
+    const handleChange = (e) => {
+        const searchValue = e.target.value;
+        if (!searchValue.startsWith(' ')) {
+            setSearchValue(searchValue);
+        }
+    };
     return (
         <HeadlessTippy
             interactive
@@ -73,7 +80,7 @@ function Search() {
                     value={searchValue}
                     placeholder="Search accounts and videos"
                     spellCheck={false}
-                    onChange={(e) => setSearchValue(e.target.value)}
+                    onChange={handleChange}
                     onFocus={() => setShowResult(true)}
                 />
                 {!!searchValue && !loading && (
