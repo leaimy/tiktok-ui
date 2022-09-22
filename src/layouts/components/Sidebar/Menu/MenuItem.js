ac90cfcc9a532e9e7afe0b9f49255b207751493c
Nguyễn Thị Hà <hantctk42@gmail.com>
2022-09-22 14:45:08 +0700

    Xây dựng UI phần sidebar #1 🌼🌼🌼

--- src/layouts/components/Sidebar/Menu/MenuItem.js
+++ src/layouts/components/Sidebar/Menu/MenuItem.js
@@ -5,10 +5,11 @@ import { NavLink } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
-function MenuItem({ title, to, icon }) {
+function MenuItem({ title, to, icon, activeIcon }) {
     return (
-        <NavLink className={cx('menu-item')} to={to}>
-            {icon}
+        <NavLink className={(nav) => cx('menu-item', { active: nav.isActive })} to={to}>
+            <span className={cx('icon')}>{icon}</span>
+            <span className={cx('active-icon')}>{activeIcon}</span>
             <span className={cx('title')}>{title}</span>
         </NavLink>
     );
@@ -18,6 +19,7 @@ MenuItem.propTypes = {
     title: PropTypes.string.isRequired,
     to: PropTypes.string.isRequired,
     icon: PropTypes.node.isRequired,
+    activeIcon: PropTypes.node.isRequired,
 };
 
 export default MenuItem;
